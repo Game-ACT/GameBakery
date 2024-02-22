32f62d02b7aa89f42e44e0544dd1bffd3d657998
noigamegun <thapat.g11@gmail.com>
2024-02-22 19:35:38 +0700

    Game

--- Company.swift
+++ Company.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 struct Company: View {
     var body: some View {
-        Text("Enter 1,3,4")
+        Text("บริษัท นาซ่าไฟร์โปรดัคส์แอนด์เซฟตี้ จำกัด")
+            .font(.system(size: 20,design: .rounded))
+            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+        Text("ทำธุรกิจเกี่ยวกับอุปกรณ์ดับเพลิง")
+            .font(.system(size: 20,design: .rounded))
+            .padding(.top, 10)
     }
 }
