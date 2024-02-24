59e4bf68d7dfa26e84106fcfbc044359e958ecd0
noigamegun <thapat.g11@gmail.com>
2024-02-24 11:42:08 +0700

    Added Product Info Page

--- Product.swift
+++ Product.swift
@@ -11,7 +11,44 @@ struct Product: View {
     var body: some View {
         ZStack {
             VStack {
-                Text("Enter number 2 here")
+                
+                /*Image("Product")
+                    .resizable()
+                    .frame(width: 250,height: 400)
+                    .aspectRatio(contentMode: .fit)
+                    .overlay(Rectangle().stroke(Color.green, lineWidth: 5))
+                    .padding(.bottom,10)
+                 */
+                
+                Text("FLAMEX")
+                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/,maxWidth: .infinity, minHeight: 50)
+                    .background(.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .font(.system(size: 23,weight: .bold))
+                    .padding(.vertical, 3)
+                    .padding(.horizontal, 10)
+                
+                Text("สเปรย์ดับเพลิงรายแรกที่เป็นแบรนด์ไทย")
+                    .font(.system(.headline))
+                    .padding(3)
+                
+                Text("สามารถดับเพลิงได้ 4 ประเภท A,B,C และ K")
+                    .font(.system(.headline))
+                    .padding(3)
+                
+                Text("เป็นนวัตกรรมเพราะเป็นสเปรย์ดับเพลิงรายแรกที่เป็นแบรนด์ไทย")
+                    .font(.system(size: 14,weight: .semibold))
+                    .padding(.top, 3)
+                Text("กลุ่มเป้าหมายคือ ผู้จัดการโรงแรม เจ้าของบ้าน เจ้าของร้าน")
+                    .font(.system(size: 14,weight: .semibold))
+                    .padding(.top, 3)
+                Image("ad1")
+                    .resizable()
+                    .frame(width: 200,height: 200)
+                    .aspectRatio(contentMode: .fit)
+                    .overlay(Rectangle().stroke(Color.green, lineWidth: 5))
+                    .padding(.vertical,20)
             }
             .navigationTitle("Product Info")
         }
